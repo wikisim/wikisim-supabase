8ca35bfcecf3d2a3a740267b356fa42652182443
Alexander James Phillips <ajp.global@gmail.com>
2025-09-17 23:20:10 +0100

    Include js_input_value in stub deno_evaluate_code_in_sandbox response

--- supabase/functions/_shared/evaluate_code_in_sandbox.deno.ts
+++ supabase/functions/_shared/evaluate_code_in_sandbox.deno.ts
@@ -9,6 +9,7 @@ export function deno_evaluate_code_in_sandbox (request: EvaluationRequest)
     // This is a stub. Replace with actual sandboxed evaluation logic
     // once we can sandbox user code in Deno.
     const response: EvaluationResponse = {
+        js_input_value: request.js_input_value,
         evaluation_id: 0,
         requested_at: request.requested_at,
         start_time: performance.now(),

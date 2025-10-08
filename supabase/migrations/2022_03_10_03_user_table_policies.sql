07bde3fe66ecd63ef6a4ef95a7f3469a7118b7b7
Alexander James Phillips <ajp.global@gmail.com>
2025-10-08 11:15:52 +0100

    Update user table policies to match DataCurator's

--- supabase/migrations/2022_03_10_03_user_table_policies.sql
+++ supabase/migrations/2022_03_10_03_user_table_policies.sql
@@ -1,8 +1,14 @@
-
+-- Copied between DataCurator/app/server/supabase_setup/3_users/0090_policy.sql
+-- and wikisim wikisim-supabase/supabase/migrations/2022_03_10_03_user_table_policies.sql repo
 
 
 alter table public.users enable row level security;
 
 
-CREATE policy "User can insert their own user entry" on public.users for insert with check ( users.id = auth.uid() );
-CREATE policy "User can update their own user entry" on public.users for update using ( users.id = auth.uid() ) with check ( users.id = auth.uid() );
+CREATE policy "User can insert their own user entry" on public.users for insert with check ( users.id = (select auth.uid()) );
+CREATE policy "User can update their own user entry" on public.users for update using ( users.id = (select auth.uid()) ) with check ( users.id = (select auth.uid()) );
+
+--- The user table currently only contains their user name so we can allow all
+--- users to read all user names
+--- SECURITY
+CREATE policy "User can read all users" on public.users for select using ( true );

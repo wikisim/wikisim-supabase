8916469f71616f1187c5963b8cd88a490194918e
Alexander James Phillips <ajp.global@gmail.com>
2025-10-10 19:33:26 +0100

    Simplify and copy public.users table RLS policies between WikiSim and DataCurator

--- supabase/migrations/2025_07_11_01_user_table_policies.sql
+++
@@ -1,4 +0,0 @@
-
--- All users and non-auth'd users to read the public users info including the trusted flag
--- SECURITY
-CREATE POLICY "User can read all users" ON public.users FOR SELECT USING ( true );

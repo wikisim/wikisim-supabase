929b1f4fe4a8f8c5c1ffcdf8fa1c6be92bc6cbce
Alexander James Phillips <ajp.global@gmail.com>
2025-10-10 19:27:59 +0100

    Use jsr import for supabase-js and add deno_get_supabase_service_role

--- supabase/functions/_shared/deno_get_supabase.ts
+++ supabase/functions/_shared/deno_get_supabase.ts
@@ -1,12 +1,11 @@
-import { createClient } from "npm:@supabase/supabase-js@2.44.2"
-// import { createClient } from "https://deno.land/x/supabase@v2.44.2/mod.ts"
+import { createClient } from "jsr:@supabase/supabase-js@2.44.2"
 
 
 import { supabase_anon_key, supabase_url } from "../_core/src/supabase/constants.ts"
 import type { Database } from "../_core/src/supabase/interface.ts"
 
 
-export function deno_get_supabase(auth_header: string)
+export function deno_get_supabase_as_user(auth_header: string)
 {
     return createClient<Database>(supabase_url, supabase_anon_key, {
         global:
@@ -17,4 +16,15 @@ export function deno_get_supabase(auth_header: string)
 }
 
 
-export type SupabaseClient = ReturnType<typeof deno_get_supabase>
+export function deno_get_supabase_service_role()
+{
+    const supabase_service_role = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")
+    if (!supabase_service_role)
+    {
+        throw new Error("SUPABASE_SERVICE_ROLE_KEY env var not set")
+    }
+    return createClient<Database>(supabase_url, supabase_service_role)
+}
+
+
+export type SupabaseClient = ReturnType<typeof deno_get_supabase_as_user>

85155bbb8a467b700bee036cd92c06d66a7e7142
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 19:08:05 +0100

    Minor change to deno_get_content_type

--- supabase/functions/_shared/deno_get_content_type.ts
+++ supabase/functions/_shared/deno_get_content_type.ts
@@ -7,6 +7,7 @@ import { contentType } from "jsr:@std/media-types"
 export function deno_get_content_type(file_path: string): string | undefined
 {
     // contentType from std/media-types only accepts the file extension
-    file_path = file_path.split(".").slice(-1)[0]
-    return contentType(file_path)
+    const file_extension = file_path.split(".").slice(-1)[0]
+    // shx should map to application/x-shx
+    return contentType(file_extension)
 }

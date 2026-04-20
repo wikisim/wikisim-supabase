71ad9a15e76a5ecd5b86ff23bb2efd19fd4e5802
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 19:45:07 +0100

    Move the file_path into extra info out of error message for Sentry

--- supabase/functions/_shared/deno_get_content_type.ts
+++ supabase/functions/_shared/deno_get_content_type.ts
@@ -4,6 +4,8 @@
 
 // deno-lint-ignore no-import-prefix
 import { contentType } from "jsr:@std/media-types@1.1.0"
+// deno-lint-ignore no-import-prefix
+import * as Sentry from "npm:@sentry/deno@10.49.0"
 
 
 export function deno_get_content_type(file_path: string): string | undefined
@@ -19,7 +21,10 @@ export function deno_get_content_type(file_path: string): string | undefined
         else if (file_extension === "shp") content_type = "application/x-shp"
         else
         {
-            console.error(`Could not determine content type for extension "${file_extension}" for file "${file_path}"`)
+            Sentry.withScope(scope => {
+                scope.setExtra("file_path", file_path)
+                Sentry.captureException(`Could not determine content type for extension "${file_extension}"`)
+            })
         }
     }
 

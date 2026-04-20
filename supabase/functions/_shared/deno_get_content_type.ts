619e9d6dfaa88b565e0120564366749c5a947490
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 19:32:04 +0100

    Log what content type is being returned for .shx

--- supabase/functions/_shared/deno_get_content_type.ts
+++ supabase/functions/_shared/deno_get_content_type.ts
@@ -2,12 +2,17 @@
 // wikisim-supabase/supabase/functions/_shared/deno_get_content_type.ts
 // wikisim-server/src/deno_get_content_type.ts
 
-import { contentType } from "jsr:@std/media-types"
+// deno-lint-ignore no-import-prefix
+import { contentType } from "jsr:@std/media-types@1.1.0"
+
 
 export function deno_get_content_type(file_path: string): string | undefined
 {
     // contentType from std/media-types only accepts the file extension
     const file_extension = file_path.split(".").slice(-1)[0]
-    // shx should map to application/x-shx
-    return contentType(file_extension)
+    const content_type = contentType(file_extension)
+
+    console.log(`Mapping "${file_path}" to content type:`, content_type)
+
+    return content_type
 }

eea5e013a4b457a9c07db3d45b66cd999610a8b1
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 19:41:34 +0100

    Handle shp and only log error on unhandled file extension type

--- supabase/functions/_shared/deno_get_content_type.ts
+++ supabase/functions/_shared/deno_get_content_type.ts
@@ -14,9 +14,13 @@ export function deno_get_content_type(file_path: string): string | undefined
 
     if (!content_type)
     {
-        console.error(`Could not determine content type for file "${file_path}" with extension "${file_extension}"`)
 
         if (file_extension === "shx") content_type = "application/x-shx"
+        else if (file_extension === "shp") content_type = "application/x-shp"
+        else
+        {
+            console.error(`Could not determine content type for extension "${file_extension}" for file "${file_path}"`)
+        }
     }
 
     return content_type
